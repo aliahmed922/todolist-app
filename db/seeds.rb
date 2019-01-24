b35462c614fe568769ea831071766d9513d8991d
Ali Ahmed <ali.ahmed.cs2014@gmail.com>
2019-01-24 17:06:09 +0500

    basic template added for todolist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Todo List"
+[
+  { text: 'Refactor drapers code', completed: false },
+  { text: 'Resolve conflicts from master branch', completed: false },
+  { text: 'Integrate StimulasJs', completed: false },
+  { text: 'Use Bootstrap 4', completed: false },
+].each do |item|
+  TodoList.find_or_create_by!(item)
+end
